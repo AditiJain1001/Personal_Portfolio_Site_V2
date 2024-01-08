464e605913cbe62aae105f89e726dc0b889bf08f
Aditi <codingwizard101@gmail.com>
2024-01-07 23:33:14 -0500

    Started working on  navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { BrowserRouter } from "react-router-dom"
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from "./components"
+
+
 const App = () => {
 
 
 
   return (
-    <h1>React dev 3d</h1>
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="ng-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar/>
+        <Hero/>
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div className="relative z-0">
+
+      </div>
+    </div>
+    </BrowserRouter>
   )
 }
 
